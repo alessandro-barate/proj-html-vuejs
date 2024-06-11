b23c8170b901fcf4c8ef17d0f44a5c29da8b00da
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-06-11 12:20:42 +0200

    [UPD] header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,24 +5,62 @@ export default {
 };
 </script>
 <template>
-  <header>
-    <ul>
-      <li>
-        <router-link :to="{ name: 'Home' }" class="classe_css">
-          Home
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'AboutUs' }" class="classe_css">
-          About Us
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'Contact' }" class="classe_css">
-          Contact
-        </router-link>
-      </li>
-    </ul>
+  <header class="py-3 bg-header-color">
+    <nav class="navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="../../public/img/logo.png"
+            alt="Logo"
+            width="100px"
+            class="d-inline-block align-text-top"
+          />
+        </a>
+        <ul class="navbar-nav list-group-horizontal">
+          <li class="mx-3 nav-item">
+            <router-link :to="{ name: 'Home' }" class="fw-bold linkheader">
+              Home
+            </router-link>
+          </li>
+          <li class="mx-3 nav-item">
+            <router-link :to="{ name: 'AboutUs' }" class="fw-bold linkheader">
+              About Us
+            </router-link>
+          </li>
+          <li class="mx-3 nav-item">
+            <router-link :to="{ name: 'Contact' }" class="fw-bold linkheader">
+              Contact
+            </router-link>
+          </li>
+          <li class="mx-3 nav-item">
+            <router-link :to="{ name: 'Home' }" class="fw-bold linkheader">
+              Home
+            </router-link>
+          </li>
+          <li class="mx-3 nav-item">
+            <router-link :to="{ name: 'AboutUs' }" class="fw-bold linkheader">
+              About Us
+            </router-link>
+          </li>
+          <li class="mx-3 nav-item">
+            <router-link :to="{ name: 'Contact' }" class="fw-bold linkheader">
+              Contact
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.bg-header-color {
+  background-color: #260262;
+}
+.linkheader {
+  text-decoration: none;
+  color: white;
+  &:hover {
+    color: #ff7a1f;
+  }
+}
+</style>
