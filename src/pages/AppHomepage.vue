44db404e07b0830b4f93848909b2fb8dd54f9b68
DanieleBernassola <danielebernassola02@gmail.com>
2024-06-11 13:16:36 +0200

    [UPD] Added smart section

--- src/pages/AppHomepage.vue
+++ src/pages/AppHomepage.vue
@@ -1,14 +1,17 @@
 <script>
-import JumboHome from '../components/JumboHome.vue'
+import JumboHome from '../components/JumboHome.vue';
+import SmartHome from '../components/SmartHome.vue';
 export default {
   name: "AppHomepage",
   components: {
     JumboHome,
+    SmartHome,
   }
 };
 </script>
 <template>
   <JumboHome></JumboHome>
+  <SmartHome></SmartHome>
 </template>
 <style scoped lang="scss">
 .page-title {
