df64aefd539d514a20968333af880f1c75442047
alessandro-barate <alebarat@gmail.com>
2024-06-11 16:48:59 +0100

    [UPD] Form section styling

--- src/pages/AppContactPage.vue
+++ src/pages/AppContactPage.vue
@@ -19,7 +19,13 @@ export default {
   },
 };
 </script>
+
+
+
+
+
 <template>
+
   <jumbotronComponent :title="Pagename" :Image="jumboImg"/>
 
   <div class="reach-us mx-auto text-center">
@@ -27,7 +33,32 @@ export default {
     <h2>How to Reach Us</h2>
     <p class="px-5">We're always interested in having a chat or discussing your next big project, so drop us a line!</p>
   </div>
+
+  <div class="container form-contacts">
+    <img class="bg-lightpink" src="../../public/img/footer/shape-top.png" alt="">
+    <div class="row bg-lightpink">
+      <div class="col-6">
+        <h3>Reach us directly!</h3>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+          <li>c</li>
+        </ul>
+      </div>
+
+      <div class="col-6">
+        <form action="">
+          <input type="text">
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
+
+
+
+
+
 <style scoped lang="scss">
 
 .reach-us {
@@ -46,6 +77,10 @@ h5 {
 h2 {
   font-size: 57px;
   font-weight: 700;
+}
+
+.bg-lightpink {
+  background-color: #F6E9FC;
 }
 
 </style>
