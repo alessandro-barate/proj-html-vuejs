4f00973d20f247e3cdd32bce7d1845c87be6ea04
alessandro-barate <alebarat@gmail.com>
2024-06-11 15:24:53 +0100

    [UPD] Styling footer reach us section

--- src/pages/AppContactPage.vue
+++ src/pages/AppContactPage.vue
@@ -21,5 +21,31 @@ export default {
 </script>
 <template>
   <jumbotronComponent :title="Pagename" :Image="jumboImg"/>
+
+  <div class="reach-us mx-auto text-center">
+    <h5>Contact Us</h5>
+    <h2>How to Reach Us</h2>
+    <p class="px-5">We're always interested in having a chat or discussing your next big project, so drop us a line!</p>
+  </div>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+.reach-us {
+  width: 50%;
+  margin-top: 80px;
+
+  p {
+    font-size: 18px;
+  }
+}
+
+h5 {
+  font-size: 24px;
+}
+
+h2 {
+  font-size: 57px;
+  font-weight: 700;
+}
+
+</style>
