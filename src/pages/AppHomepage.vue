0668f4a4c66379b2104fb8ce540cf64adbce867d
DanieleBernassola <danielebernassola02@gmail.com>
2024-06-11 16:15:02 +0200

    [UPD] Added counter component

--- src/pages/AppHomepage.vue
+++ src/pages/AppHomepage.vue
@@ -1,16 +1,19 @@
 <script>
 import JumboHome from '../components/JumboHome.vue';
 import SmartComponent from '../components/SmartComponent.vue';
+import Countercomp from '../components/countercomp.vue';
 export default {
   name: "AppHomepage",
   components: {
     JumboHome,
     SmartComponent,
+    Countercomp,
   }
 };
 </script>
 <template>
   <JumboHome></JumboHome>
   <SmartComponent></SmartComponent>
+  <Countercomp></Countercomp>
 </template>
 <style scoped lang="scss"></style>
