400d0d386f3950b626932ceb4225ad2afedc8e8c
alessandro-barate <alebarat@gmail.com>
2024-06-11 18:23:38 +0100

    [UPD] Styling form

--- src/pages/AppContactPage.vue
+++ src/pages/AppContactPage.vue
@@ -37,7 +37,7 @@ export default {
   <div class="container form-contacts">
     <img class="bg-lightpink" src="../../public/img/footer/shape-top.png" alt="">
     <div class="row bg-lightpink">
-      <div class="col-6">
+      <div class="col-6 left-side">
         <h3>Reach us directly!</h3>
         <ul>
           <li class="d-flex">
@@ -68,9 +68,33 @@ export default {
       </div>
 
       <div class="col-6">
-        <form action="">
-          <input type="text">
-        </form>
+
+        <div class="form-container">
+        
+          <div class="text-center">
+            <h2>Contact form</h2>
+            <p>Tell us your story and let's make it awesome</p>
+          </div>
+        
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputText1" class="form-label">s</label>
+              <input type="text" class="form-control" id="exampleInputText1">
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -98,10 +122,15 @@ h2 {
 
 h3 {
   font-size: 43px;
+  font-weight: bold;
+  margin-bottom: 40px;
 }
 
 h5 {
   font-size: 24px;
+  padding-top: 20px;
+  line-height: 25px;
+  font-weight: bold;
 }
 
 .col-6 ul {
@@ -155,6 +184,10 @@ ul li {
 
 #width-40 {
   width: 40%;
+}
+
+.left-side {
+  font-weight: bold;
 }
 
 </style>
