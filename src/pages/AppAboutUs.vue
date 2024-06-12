71d2a86a8cd335df2d442a05868dbd6d4de9e514
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-06-12 11:22:11 +0200

    [UPD]ADDED STORYTELLING AND  SMART COMP TO ABOUT US

--- src/pages/AppAboutUs.vue
+++ src/pages/AppAboutUs.vue
@@ -2,6 +2,8 @@
 import jumbotronComponent from "../components/jumbotronAboutUs.vue";
 import counterComp from "../components/countercomp.vue";
 import ourServicecomp from "../components/ourServicecomp.vue";
+import smartComponent from "../components/SmartComponent.vue";
+import storyTelling from "../components/AppStorytelling.vue";
 export default {
   name: "AppAboutUs",
   data() {
@@ -17,13 +19,17 @@ export default {
     jumbotronComponent,
     counterComp,
     ourServicecomp,
+    smartComponent,
+    storyTelling,
   },
   methods: {},
 };
 </script>
 <template>
   <jumbotronComponent :title="Pagename" :Image="jumboImg"></jumbotronComponent>
+  <smartComponent></smartComponent>
   <counterComp></counterComp>
   <ourServicecomp></ourServicecomp>
+  <storyTelling></storyTelling>
 </template>
 <style scoped lang="scss"></style>
